b94856bfddb27acc2e9222289a12e7dd8270b701
AaronLeeeee <ljhumane002@hotmail.com>
2016-11-08 13:28:29 +0800

    支持JQuery

--- AMDDemo/Modules/TestModule.js
+++ AMDDemo/Modules/TestModule.js
@@ -3,8 +3,8 @@
  */
 
 define(function (require) {
-    var add = require('AddModule');
-    var multiply = require('MultiplyModule');
+    var add = require('./AddModule');
+    var multiply = require('./MultiplyModule');
 
     return function (x, y) {
       return add(x, y) * multiply(x, y);
